b63347dae6dc1c3f5672c0963eaf3d50f3aca415
LeeSuA <huimhuimi@naver.com>
2023-06-10 21:08:48 +0900

    [PROG]_LV2_요격_시스템

---
+++ PROG/LV2_요격_시스템.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        
+        Arrays.sort(targets, (o1, o2) -> {
+            if(o1[1] == o2[1])  return o1[0] - o2[0];
+            return o1[1] - o2[1];
+        });
+        int last = -1;
+        for(int[] target : targets){
+            if(last == -1){
+                answer++;
+                last = target[1] - 1;
+                continue;
+            }
+            
+            if(last >= target[0] && last <= target[1]) continue;
+            answer++;
+            last = target[1] -1;
+        }
+
+        return answer;
+    }
+}
