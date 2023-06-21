61fa4782325504ee8757140b009a9692064259fe
LeeSuA <huimhuimi@naver.com>
2023-06-21 18:21:46 +0900

    [PROG]_LV1_가운데_글자_가져오기

---
+++ PROG/LV1_가운데_글자_가져오기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int length = s.length();
+        if(length%2 == 0) answer = String.valueOf(s.charAt(length/2-1)) + String.valueOf(s.charAt(length/2));
+        else answer = String.valueOf(s.charAt(length/2));
+        return answer;
+    }
+}
