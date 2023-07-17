bb9365c14d28ee648150c93507aaaa5820a27628
LeeSuA <huimhuimi@naver.com>
2023-07-17 15:42:05 +0900

    [PROG]_LV1_음양_더하기

---
+++ PROG/LV1_음양_더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0, size = absolutes.length; i < size; i++){
+            if(signs[i])    answer = answer + absolutes[i];
+            else answer = answer - absolutes[i];
+        }
+        return answer;
+    }
+}
