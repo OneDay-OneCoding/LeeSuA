b5c14d974f18684284bc3dd33a0ee7c5935f377d
LeeSuA <huimhuimi@naver.com>
2023-07-27 15:56:20 +0900

    [PROG]_LV1_콜라츠_추측

---
+++ PROG/LV1_콜라츠_추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        while(num != 1){
+            answer++;
+            if(num%2 == 1) {
+                num = num*3 + 1;
+            }
+            
+            else if(num%2 == 0){
+                num = num / 2;
+            }
+            
+            if(answer > 500){       
+                answer = -1;
+                break;
+            }
+        }
+
+    
+        return answer;
+    }
+}
